a695502379a37d7f77dc04085e19a4e8eec00d6c
Will Wright <will@wright.is>
2014-10-05 23:25:52 +0100

    Added Avgrund Modal

--- js/scripts.js
+++ js/scripts.js
@@ -141,3 +141,30 @@ $(window).load(function() {
 	});
 
 });
+
+/***************** Avgrund for Portfolio ******************/
+
+$(function() {
+    $('.expand.first').avgrund({
+      width: 640, // max is 640px
+      height: 350, // max is 350px
+      showClose: true, // switch to 'true' for enabling close button
+      showCloseText: 'x', // type your text for close button
+      closeByEscape: true, // enables closing popup by 'Esc'..
+      closeByDocument: false, // ..and by clicking document itself
+      holderClass: 'avgrund', // lets you name custom class for popin holder..
+      // overlayClass: '', // ..and overlay block
+      // enableStackAnimation: false, // another animation type
+      onBlurContainer: 'body', // enables blur filter for specified block
+      openOnEvent: true, // set to 'false' to init on load
+      setEvent: 'click', // use your event like 'mouseover', 'touchmove', etc.
+      // onLoad: function (elem) { ... }, // set custom call before popin is inited..
+      // onUnload: function (elem) { ... }, // ..and after it was closed
+      template: '<p>So implement your design and place content here! If you want to close modal, please hit "Esc", click somewhere on the screen or use special button.</p>' +
+      '<div>' +
+      '<a href="http://github.com/voronianski/jquery.avgrund.js" target="_blank" class="github">Avgrund on Github</a>' +
+      '<a href="http://twitter.com/voronianski" target="_blank" class="twitter">Twitter</a>' +
+      '<a href="http://dribbble.com/voronianski" target="_blank" class="dribble">Dribbble</a>' +
+      '</div>'
+    });
+  });
